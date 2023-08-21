d783778accf22b8eaf985ae7e6a6bbc2e40b8196
Vardhu2706 <ayyagari.vardhu@gmail.com>
2023-08-21 13:51:35 +0530

    Added Auth Middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,11 +6,16 @@ import {
   updateUserProfile,
 } from "../controllers/userController.js";
 import express from "express";
+import { protect } from "../middleware/authMiddleware.js";
+
 const router = express.Router();
 
 router.post("/", registerUser);
 router.post("/auth", authUser);
 router.post("/logout", logoutUser);
-router.route("/profile").put(updateUserProfile).get(getUserProfile);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
 export default router;
